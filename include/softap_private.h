ecca1d9d6e47e6e897f957fa444461842f7cd315
Seonah Moon <seonah1.moon@samsung.com>
2016-02-03 14:46:23 +0900

    Initialize softap package
    Change-Id: I36a3e3c74c4af0ce1ed36861e3a5ec6f5111ad11
    Signed-off-by: Seonah Moon <seonah1.moon@samsung.com>

---
+++ include/softap_private.h
@@ -0,0 +1,270 @@
+/*
+* Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef __SOFTAP_PRIVATE_H__
+#define __SOFTAP_PRIVATE_H__
+
+#define LOG_TAG	"CAPI_NETWORK_SOFTAP"
+
+#include <glib.h>
+#include <dlog.h>
+#include <gio/gio.h>
+#include "softap.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef API
+#define API __attribute__ ((visibility("default")))
+#endif
+
+#ifndef DEPRECATED_API
+#define DEPRECATED_API __attribute__ ((deprecated))
+#endif
+
+#define DBG(fmt, args...)	LOGD(fmt, ##args)
+#define WARN(fmt, args...)	LOGW(fmt, ##args)
+#define ERR(fmt, args...)	LOGE(fmt, ##args)
+#define SDBG(fmt, args...)	SECURE_LOGD(fmt, ##args)
+#define SERR(fmt, args...)	SECURE_LOGE(fmt, ##args)
+
+#define _warn_if(expr, fmt, arg...) do { \
+		if (expr) { \
+			WARN(fmt, ##arg); \
+		} \
+	} while (0)
+
+#define _ret_if(expr) do { \
+		if (expr) { \
+			return; \
+		} \
+	} while (0)
+
+#define _retv_if(expr, val) do { \
+		if (expr) { \
+			return (val); \
+		} \
+	} while (0)
+
+#define _retm_if(expr, fmt, arg...) do { \
+		if (expr) { \
+			ERR(fmt, ##arg); \
+			return; \
+		} \
+	} while (0)
+
+#define _retvm_if(expr, val, fmt, arg...) do { \
+		if (expr) { \
+			ERR(fmt, ##arg); \
+			return (val); \
+		} \
+	} while (0)
+
+#define CHECK_FEATURE_SUPPORTED(...) \
+	do { \
+		int rv = _softap_check_feature_supported(__VA_ARGS__, NULL); \
+		if(rv != SOFTAP_ERROR_NONE) { \
+			return rv; \
+		} \
+	} while (0)
+
+int _softap_check_feature_supported(const char* feature, ...);
+
+/**
+* Start of mobileap-agent common values
+* When these values are changed, mobileap-agent should be also changed.
+* But some of those will be removed.
+*/
+
+/*
+* from mobileap_lib.h
+*/
+
+/**
+* Common configuration
+*/
+#define SOFTAP_STR_INFO_LEN		20	/**< length of the ip or mac address */
+
+/**
+* Mobile AP error code
+*/
+typedef enum {
+	MOBILE_AP_ERROR_NONE,			/**< No error */
+	MOBILE_AP_ERROR_RESOURCE,		/**< Socket creation error, file open error */
+	MOBILE_AP_ERROR_INTERNAL,		/**< Driver related error */
+	MOBILE_AP_ERROR_INVALID_PARAM,		/**< Invalid parameter */
+	MOBILE_AP_ERROR_ALREADY_ENABLED,	/**< Mobile AP is already ON */
+	MOBILE_AP_ERROR_NOT_ENABLED,		/**< Mobile AP is not ON, so cannot be disabled */
+	MOBILE_AP_ERROR_NET_OPEN,		/**< PDP network open error */
+	MOBILE_AP_ERROR_NET_CLOSE,		/**< PDP network close error */
+	MOBILE_AP_ERROR_DHCP,			/**< DHCP error */
+	MOBILE_AP_ERROR_IN_PROGRESS,		/**< Request is in progress */
+	MOBILE_AP_ERROR_NOT_PERMITTED,		/**< Operation is not permitted */
+	MOBILE_AP_ERROR_PERMISSION_DENIED,  /**< Permission Denied */
+
+	MOBILE_AP_ERROR_MAX
+} mobile_ap_error_code_e;
+
+/**
+* Event type on callback
+*/
+typedef enum {
+	MOBILE_AP_ENABLE_CFM = 0,
+	MOBILE_AP_DISABLE_CFM = 1,
+
+	MOBILE_AP_ENABLE_WIFI_AP_CFM = 9,
+	MOBILE_AP_DISABLE_WIFI_AP_CFM,
+
+	MOBILE_AP_GET_STATION_INFO_CFM,
+	MOBILE_AP_GET_DATA_PACKET_USAGE_CFM
+} mobile_ap_event_e;
+
+typedef enum {
+	MOBILE_AP_TYPE_WIFI,
+	MOBILE_AP_TYPE_USB,
+	MOBILE_AP_TYPE_BT,
+	MOBILE_AP_TYPE_WIFI_AP,
+	MOBILE_AP_TYPE_MAX,
+} mobile_ap_type_e;
+
+typedef enum {
+	E_SIGNAL_NET_CLOSED = 0,
+	E_SIGNAL_WIFI_TETHER_ON,
+	E_SIGNAL_WIFI_TETHER_OFF,
+	E_SIGNAL_WIFI_AP_ON = 9,
+	E_SIGNAL_WIFI_AP_OFF,
+	E_SIGNAL_NO_DATA_TIMEOUT,
+	E_SIGNAL_LOW_BATTERY_MODE,
+	E_SIGNAL_FLIGHT_MODE,
+	E_SIGNAL_SECURITY_TYPE_CHANGED,
+	E_SIGNAL_SSID_VISIBILITY_CHANGED,
+	E_SIGNAL_PASSPHRASE_CHANGED,
+	E_SIGNAL_DHCP_STATUS,
+	E_SIGNAL_MAX
+} mobile_ap_sig_e;
+
+#define SOFTAP_SERVICE_OBJECT_PATH	"/Tethering"
+#define SOFTAP_SERVICE_NAME		"org.tizen.tethering"
+#define SOFTAP_SERVICE_INTERFACE	"org.tizen.tethering"
+
+#define SOFTAP_SIGNAL_MATCH_RULE	"type='signal',interface='org.tizen.tethering'"
+#define SOFTAP_SIGNAL_NAME_LEN	64
+
+#define SIGNAL_NAME_NET_CLOSED		"net_closed"
+#define SIGNAL_NAME_STA_CONNECT		"sta_connected"
+#define SIGNAL_NAME_STA_DISCONNECT	"sta_disconnected"
+#define SIGNAL_NAME_WIFI_AP_ON		"wifi_ap_on"
+#define SIGNAL_NAME_WIFI_AP_OFF		"wifi_ap_off"
+#define SIGNAL_NAME_NO_DATA_TIMEOUT	"no_data_timeout"
+#define SIGNAL_NAME_LOW_BATTERY_MODE	"low_batt_mode"
+#define SIGNAL_NAME_FLIGHT_MODE		"flight_mode"
+#define SIGNAL_NAME_SECURITY_TYPE_CHANGED	"security_type_changed"
+#define SIGNAL_NAME_SSID_VISIBILITY_CHANGED	"ssid_visibility_changed"
+#define SIGNAL_NAME_PASSPHRASE_CHANGED		"passphrase_changed"
+#define SIGNAL_NAME_DHCP_STATUS		"dhcp_status"
+
+#define SIGNAL_MSG_NOT_AVAIL_INTERFACE	"Interface is not available"
+#define SIGNAL_MSG_TIMEOUT		"There is no connection for a while"
+#define SIGNAL_MSG_SSID_VISIBLE		"ssid_visible"
+#define SIGNAL_MSG_SSID_HIDE		"ssid_hide"
+
+/* Network Interface */
+#define SOFTAP_SUBNET_MASK		"255.255.255.0"
+
+#define SOFTAP_IF		"wlan0"
+#define SOFTAP_GATEWAY		"192.168.43.1"
+
+#define SOFTAP_SSID_MAX_LEN	32	/**< Maximum length of ssid */
+#define SOFTAP_KEY_MIN_LEN	8	/**< Minimum length of wifi key */
+#define SOFTAP_KEY_MAX_LEN	64	/**< Maximum length of wifi key */
+#define SOFTAP_HASH_KEY_MAX_LEN	64
+
+#define SOFTAP_MODE_MAX_LEN 10  /**< Maximum length of mode */
+
+#define VCONFKEY_MOBILE_HOTSPOT_SSID	"memory/private/mobileap-agent/ssid"
+#define MAX_ALIAS_LEN	256
+
+/**
+* End of mobileap-agent common values
+*/
+
+#define SOFTAP_DEFAULT_SSID	"Tizen"
+#define SOFTAP_SECURITY_TYPE_OPEN_STR		"open"
+#define SOFTAP_SECURITY_TYPE_WPA2_PSK_STR	"wpa2-psk"
+#define SOFTAP_ERROR_RECOVERY_MAX			3
+#define SECURITY_TYPE_LEN	32
+#define PSK_ITERATION_COUNT	4096
+
+typedef void (*__handle_cb_t)(GDBusConnection *connection, const gchar *sender_name,
+		const gchar *object_path, const gchar *interface_name, const gchar *signal_name,
+		GVariant *parameters, gpointer user_data);
+
+typedef struct {
+	int sig_id;
+	char name[SOFTAP_SIGNAL_NAME_LEN];
+	__handle_cb_t cb;
+} __softap_sig_t;
+
+typedef struct {
+	GDBusConnection *client_bus;
+	GDBusProxy *client_bus_proxy;
+	GCancellable *cancellable;
+
+	softap_enabled_cb enabled_cb[SOFTAP_TYPE_MAX];
+	void *enabled_user_data[SOFTAP_TYPE_MAX];
+	softap_disabled_cb disabled_cb[SOFTAP_TYPE_MAX];
+	void *disabled_user_data[SOFTAP_TYPE_MAX];
+	softap_connection_state_changed_cb changed_cb[SOFTAP_TYPE_MAX];
+	void *changed_user_data[SOFTAP_TYPE_MAX];
+	softap_data_usage_cb data_usage_cb;
+	void *data_usage_user_data;
+	softap_settings_reloaded_cb ap_settings_reloaded_cb;
+	void *ap_settings_reloaded_user_data;
+	char *ap_ssid;
+	char passphrase[SOFTAP_WIFI_KEY_MAX_LEN + 1];
+	softap_security_type_e sec_type;
+	bool visibility;
+} __softap_h;
+
+typedef struct {
+	softap_type_e interface;			/**< interface type */
+	char ip[SOFTAP_STR_INFO_LEN];		/**< assigned IP address */
+	char mac[SOFTAP_STR_INFO_LEN];		/**< MAC Address */
+	char *hostname;
+	time_t tm;	/**< connection time */
+} __softap_client_h;
+
+typedef struct {
+	softap_type_e interface;			/**< interface type */
+	char interface_name[SOFTAP_STR_INFO_LEN];	/**< interface alphanumeric name */
+	char ip_address[SOFTPAP_STR_INFO_LEN];	/**< assigned ip addresss to interface */
+	char gateway_address[SOFTAP_STR_INFO_LEN];	/**< gateway address of interface */
+	char subnet_mask[SOFTAP_STR_INFO_LEN];	/**< subnet mask of interface */
+} __softap_interface_t;
+
+typedef struct {
+	char ssid[SOFTAP_SSID_MAX_LEN + 1];
+	char key[SOFTAP_KEY_MAX_LEN + 1];
+	softap_security_type_e sec_type;
+	bool visibility;
+} _softap_settings_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SOFTAP_PRIVATE_H__ */
---
+++ packaging/capi-network-softap.spec
@@ -0,0 +1,75 @@
+Name:		capi-network-softap
+Summary:	Softap Framework
+Version:	0.0.1
+Release:	1
+Group:		System/Network
+License:	Apache-2.0
+Source0:	%{name}-%{version}.tar.gz
+BuildRequires:	pkgconfig(dlog)
+BuildRequires:	pkgconfig(dbus-1)
+BuildRequires:	pkgconfig(capi-base-common)
+BuildRequires:	pkgconfig(glib-2.0)
+BuildRequires:	pkgconfig(gio-2.0)
+BuildRequires:	pkgconfig(vconf)
+BuildRequires:	pkgconfig(libssl)
+BuildRequires:	pkgconfig(capi-system-info)
+BuildRequires:	cmake
+Requires(post):		/sbin/ldconfig
+Requires(postun):	/sbin/ldconfig
+
+%description
+Soft AP framework library for CAPI
+
+%package devel
+Summary:	Development package for Soft AP framework library
+Group:		Development/Libraries
+Requires:	%{name} = %{version}-%{release}
+%description devel
+Development package for Tethering framework library
+
+%prep
+%setup -q
+
+
+%build
+export CFLAGS="$CFLAGS -DTIZEN_DEBUG_ENABLE"
+export CXXFLAGS="$CXXFLAGS -DTIZEN_DEBUG_ENABLE"
+export FFLAGS="$FFLAGS -DTIZEN_DEBUG_ENABLE"
+%endif
+	.
+
+make %{?_smp_mflags}
+
+%install
+%make_install
+
+mkdir -p %{buildroot}/usr/share/license
+cp LICENSE.APLv2.0 %{buildroot}/usr/share/license/capi-network-softap
+cp LICENSE.APLv2.0 %{buildroot}/usr/share/license/capi-network-softap-devel
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+
+%files
+%manifest capi-network-softap.manifest
+%defattr(-,root,root,-)
+%{_libdir}/*.so.*
+/usr/share/license/capi-network-softap
+%{_bindir}/softap_test
+%ifarch %{arm}
+/etc/config/connectivity/sysinfo-softap.xml
+%else
+%if 0%{?simulator}
+# Noop
+%else
+/etc/config/connectivity/sysinfo-softap.xml
+%endif
+%endif
+
+%files devel
+%defattr(-,root,root,-)
+%{_includedir}/network/*.h
+%{_libdir}/pkgconfig/*.pc
+%{_libdir}/*.so
+/usr/share/license/capi-network-softap-devel
